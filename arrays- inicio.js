9156f20134a7cf26b65faa1ca82bb0d643a09aa6
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-05-02 17:53:01 -0300

    Add files via upload

--- arrays- inicio.js
+++ arrays- inicio.js
@@ -18,3 +18,19 @@ let r = nomes1.splice(1,1,"teo")
 nomes1.push("ana")
 let d = nomes1.splice(nomes1.length+1,0,"Ana")
 //['jose', 'teo', 'maria', 'bia', 'ana', 'Ana']0: "jose"1: "teo"2: "maria"3: "bia"4: "ana"5: "Ana"
+
+let alunos = [
+    {nome: "Zé", idade: 43},
+    {nome: "Yuri", idade: 34},
+    {nome: "Cesar", idade: 38},
+    {nome: "Mario", idade:78}
+    ];
+    alunos.sort (function (a,b) {
+        if(a.nome > b.nome){
+            return -1;
+        }
+        if(a.nome < b.nome){
+            return 1;
+        }
+    })
+    
