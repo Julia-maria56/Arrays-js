233c777b528d1126dbb3cb94e8d1239bcf51a2e0
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-05-02 17:48:26 -0300

    Add files via upload

---
+++ arrays- inicio.js
@@ -0,0 +1,20 @@
+let nomes = ["jose","maria","joão"]
+nomes.push("bia")
+//['jose', 'maria', 'joão', 'bia']
+nomes.pop()
+//['jose', 'maria', 'joão']
+
+let a = [2, 4,1,8]
+a.shift()
+//[4, 1, 8]
+
+let b = ["julia", "allan", "taina", "isabelly"]
+let remove = a.splice(1,1,"enzo")
+//['julia', 'enzo', 'taina', 'isabelly']
+
+let nomes1 = ["jose","joao","maria","bia"]
+let r = nomes1.splice(1,1,"teo")
+//['jose', 'teo', 'maria', 'bia']
+nomes1.push("ana")
+let d = nomes1.splice(nomes1.length+1,0,"Ana")
+//['jose', 'teo', 'maria', 'bia', 'ana', 'Ana']0: "jose"1: "teo"2: "maria"3: "bia"4: "ana"5: "Ana"
