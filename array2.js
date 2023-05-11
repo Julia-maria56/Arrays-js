e1069bb02a784333c8687127b4c34b89a5f1cc58
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-05-11 14:19:51 -0300

    Add files via upload

---
+++ array2.js
@@ -0,0 +1,20 @@
+let nomes = ["Zé","Ana","Teo","Bia"]
+let aproados = nomes.slice(1,2)
+console.log(aproados)
+
+var resultado=nomes.concat('Maria')
+resultado=nomes.concat([1,2,3], 'Léo')
+
+let frutas=['Uva', 'Maçã', 'Pera']
+
+let fru = frutas.join("-")
+'Uva-Maçã-Pera'
+let res = frutas.concat([3,6,9],"Zé")
+ ['Uva', 'Maçã', 'Pera', 3, 6, 9, 'Zé']
+
+var pos=frutas.indexOf('Uva')  
+var posi =frutas.lastIndexOf("Uva") 
+
+var posi =frut.lastIndexOf("Uva") 
+
+
